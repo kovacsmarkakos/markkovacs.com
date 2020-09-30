cab604e041a0638ac772a7b927235016efda929e
Mark Kovacs <64911445+kovacsmarkakos@users.noreply.github.com>
2020-09-30 23:26:58 +0200

    name change

--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -1,4 +1,4 @@
 export * from './Hero';
-export * from './Skills';
+export * from './About';
 export * from './Contact';
 export * from './Projects';
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Layout, SEO } from 'components/common';
-import { Hero, Skills, Contact, Projects } from 'components/landing';
+import { Hero, About, Contact, Projects } from 'components/landing';
 
 export default () => (
   <Layout>
     <SEO />
     <Hero />
     <Projects />
-    <Skills />
+    <About />
     <Contact />
   </Layout>
 );
